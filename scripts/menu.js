a60eaf092a04239a85a8fd7d8ad4447d41057656
fre-denni <fredenni.git@gmail.com>
2025-08-26 18:05:10 +0200

    added basic modal js and css

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,6 @@
-///////////////////////////////////////////////
 /**********************************************
- * 
  * Functional Script that handles the menu
  * -> search and query of results (highlighting elements) -- trigger with d3?
  * -> open and close modal window
  * -> open to the website of the project
- * 
- * 
 ***********************************************/
-////////////////////////////////////////////////
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,29 @@
-///////////////////////////////////////////////
 /**********************************************
- * 
  * Functional Script that handles the modal
  * -> manage graphic and content depending on visualisation
- * 
- * 
- * 
 ***********************************************/
-////////////////////////////////////////////////
+
+// Get the modal element (div, menu button, x,  and content)
+let modal = document.getElementById("legend-modal");
+let btn = document.getElementById("menu-legend");
+let span = document.getElementById("modal-close");
+let content = document.getElementById("modal-content");
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//manage content of the modal
