c34934ea6a9fa215d432104c1f413af33d88406b
fre-denni <fredenni.git@gmail.com>
2025-08-26 17:12:40 +0200

    Adjusted spelling error

---
+++ scripts/createComparisonsTool.js
@@ -0,0 +1,28 @@
+/********************************************
+ * 
+ *
+ * 
+ * 
+ ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▖  ▗▄▖ ▗▄▄▖ ▗▄▄▄▖ ▗▄▄▖ ▗▄▖ ▗▖  ▗▖    ▗▄▄▄▖▗▄▖  ▗▄▖ ▗▖   
+▐▌   ▐▌ ▐▌▐▛▚▞▜▌▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌  █  ▐▌   ▐▌ ▐▌▐▛▚▖▐▌      █ ▐▌ ▐▌▐▌ ▐▌▐▌   
+▐▌   ▐▌ ▐▌▐▌  ▐▌▐▛▀▘ ▐▛▀▜▌▐▛▀▚▖  █   ▝▀▚▖▐▌ ▐▌▐▌ ▝▜▌      █ ▐▌ ▐▌▐▌ ▐▌▐▌   
+▝▚▄▄▖▝▚▄▞▘▐▌  ▐▌▐▌   ▐▌ ▐▌▐▌ ▐▌▗▄█▄▖▗▄▄▞▘▝▚▄▞▘▐▌  ▐▌      █ ▝▚▄▞▘▝▚▄▞▘▐▙▄▄▖
+
+    * Designed by Federico Denni & Chiara di Lodovico
+    * Coded by Federico Denni for Project TRAMA
+    * seen on ichatlas.com
+    * Politecnico di Milano - Design Department
+    * iCoolHunt Spa
+ *                                                                               
+ *                                                                               
+ *                                                                               
+ **********************************************/
+
+// Thanks to Visual Cinnamon, Observable and Shirley Wu for inspiration
+// This tool is built upon d3.js and bboxCollide libraries, and help users
+// to dinamically compare the data from the qualitative research on best practices in ICH projects
+
+
+const createComparisonsTool = (container, data) => {
+    
+}
