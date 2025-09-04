4afa280e7ca6a5a37fba45c23b7feadfc0fc0a8c
fre-denni <fredenni.git@gmail.com>
2025-09-04 15:25:06 +0200

    adjusted css position in project tree to easier workflow

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,4 +3,27 @@
  * -> search and query of results (highlighting elements) -- trigger with d3?
  * -> open and close modal window
  * -> open to the website of the project
-***********************************************/
+ ***********************************************/
+
+// search dataset function (triggered on main code)
+
+// Show labels and content modal on menu elements
+
+// download dataset when activated (triggered on main code)
+
+// check if it is in iframe and hide "more" checkbox
+function checkIframe() {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+}
+
+if (checkIframe()) {
+  //element to not display when in <iframe>
+  document.getElementById("more").style.display = "none";
+
+  //Opzione 2: aggiungi a tutti gli elementi un "in-an-iframe"
+  //class e rendi il default "display none" con CSS se JS fallisce
+}
