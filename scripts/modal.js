3b128880d453d384222bb3a54f1738212ec174c6
fre-denni <fredenni.git@gmail.com>
2025-09-05 17:10:02 +0200

    adjusted hover/checked bug for the search, info and download buttons

--- scripts/modal.js
+++ scripts/modal.js
@@ -25,12 +25,14 @@ document.addEventListener("DOMContentLoaded", function () {
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = "none";
+    btn.checked = false;
   };
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
+      btn.checked = false;
     }
   };
 
