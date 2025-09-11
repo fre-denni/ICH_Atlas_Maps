5a4ec180e2e9af15c7b8a629235db745bfdc354e
fre-denni <fredenni.git@gmail.com>
2025-09-11 12:19:41 +0200

    added basic variables

--- scripts/createComparisonsTool.js
+++ scripts/createComparisonsTool.js
@@ -12,7 +12,7 @@
     * Data researched and classified by Federica Rubino & Chiara di Lodovico
     * Coded by Federico Denni for Project TRAMA
     * seen on ichatlas.com
-    * Politecnico di Milano - Design Department
+    * Politecnico di Milano - Design Department - Coordinated by Davide Spallazzo
     * iCoolHunt Spa
  *                                                                               
  *                                                                               
@@ -23,4 +23,25 @@
 // This tool is built upon d3.js and bboxCollide libraries, and help users
 // to dinamically compare the data from the qualitative research on best practices in ICH projects
 
-const createComparisonsTool = (container, data) => {};
+const createComparisonsTool = (container, data) => {
+  //////////////////////////////////////
+  ////// Constants & Variables ////////
+  /////////////////////////////////////
+
+  //define math variables
+  const PI = Math.PI;
+  const TAU = PI * 2;
+
+  let round = Math.round;
+  let cos = Math.cos;
+  let sin = Math.sin;
+  let sqrt = Math.sqrt;
+  let min = Math.min;
+  let max = Math.max;
+  let abs = Math.abs;
+
+  //set sizes
+  const MIN_WIDTH = 660;
+  let width = max(MIN_WIDTH, container.clientWidth);
+  let height = container.clientHeight;
+};
--- scripts/createCreativeMap.js
+++ scripts/createCreativeMap.js
@@ -12,7 +12,7 @@
     * Data researched and classified by Federica Rubino & Chiara di Lodovico
     * Coded by Federico Denni for Project TRAMA
     * seen on ichatlas.com
-    * Politecnico di Milano - Design Department
+    * Politecnico di Milano - Design Department - Coordinated by Davide Spallazzo
     * iCoolHunt Spa
  *                                                                               
  *                                                                               
@@ -24,4 +24,46 @@
 // how data and projects are shaped depending on different creative approaches and methodologies
 // more on creative approaches at -> inserire link to paper
 
-const createCreativeMap = (container, data) => {};
+const createCreativeMap = (container, data) => {
+  //////////////////////////////////////
+  ////// Constants & Variables ////////
+  /////////////////////////////////////
+
+  //define math variables
+  const PI = Math.PI;
+  const TAU = PI * 2;
+
+  let round = Math.round;
+  let cos = Math.cos;
+  let sin = Math.sin;
+  let sqrt = Math.sqrt;
+  let min = Math.min;
+  let max = Math.max;
+  let abs = Math.abs;
+
+  //set sizes
+  const MIN_WIDTH = 660;
+  let width = max(MIN_WIDTH, container.clientWidth);
+  let height = container.clientHeight;
+
+  //create svg container
+  const svg = d3
+    .select(container)
+    .append("svg")
+    .attr("id", "data-visualisation")
+    .style("display", "block")
+    .attr("width", width)
+    .attr("height", height)
+    .style("background-color", "#fff")
+    .style("margin", "0");
+
+  //to handle resizing automatically
+  //https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
+  // to see later
+
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+  /** Da vedere impostazione e connessione data */
+};
