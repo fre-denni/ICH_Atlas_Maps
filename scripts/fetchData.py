d5700c50b4fedd327bd436c20d0103fb59a333cb
fre-denni <fredenni.git@gmail.com>
2025-09-15 17:32:54 +0200

    updated script with cleaning routine (cleans NAN values)

--- scripts/fetchData.py
+++ scripts/fetchData.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as numpy
+import numpy as np
 import os
 import time
 import requests
@@ -21,6 +21,7 @@ for index, row in ldf.iterrows():
 
 # Use the list to periodically check if there are modifications to the file, if yes download it
   try:
+    print(f"Downloading data from {url}...")
     response = requests.get(url, timeout=15)
     response.raise_for_status()
 
@@ -28,10 +29,28 @@ for index, row in ldf.iterrows():
       f.write(response.text)
 
     print(f"Saved to '{download_path}'")
+
+    # Step 2 : Clean data
+    # Clean the null values, parse (if needed) and save file to directory "/data"
+
+    try:
+      clean = pd.read_csv(download_path, na_values=['NA', '["NA"]'])
+      #keep track of original row count
+      og_rows = len(clean)
+
+      #drop rows with NAN
+      clean.dropna(inplace=True)
+
+      #keep track of new row count
+      cleaned_rows = len(clean)
+
+      #Save the file and overwrite it (index=False won't create a new column)
+      clean.to_csv(download_path, index=False, encoding='utf-8')
+      print(f"Cleaning complete. Removed {og_rows - cleaned_rows} rows.")
+    except pd.errors.EmptyDataError:
+      print(f"Warning: {donwload_path} is empty. Skipping cleaning")
+    except Exception as e:
+      print(f"An error occured while cleaning {file_name}.csv: {e}")
   
   except requests.exceptions.RequestException as e:
     print(f"Error downloading {url}: {e}")
-
-
-# Step 2 : Clean data
-# Clean the null values, parse (if needed) and save file to directory "/data"
