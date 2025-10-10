0ae6129e97f3359dc9df0da4e3f302a57702800b
fre-denni <fredenni.git@gmail.com>
2025-10-10 16:27:18 +0200

    added resize and params logic for visualisation code

--- scripts/createCompetencesMap.js
+++ scripts/createCompetencesMap.js
@@ -23,8 +23,12 @@
 // This tool is built upon d3.js and bboxCollide libraries, and visualises
 // the mapping of competences and technologies required to different fruitions output of projects
 
-const createCompetencesMap = (container, dataset1, dataset2, dataset3) => {
-  /**
+//to handle resizing automatically
+//https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
+// to see later
+
+const createCompetencesMap = (container) => {
+  /***
    *
    *
    *
@@ -45,12 +49,40 @@ const createCompetencesMap = (container, dataset1, dataset2, dataset3) => {
   let max = Math.max;
   let abs = Math.abs;
 
-  /////////////////////DATASETS///////////////////////
+  /////////set sizes
+  let width, height;
+  let angle;
 
-  //populate datasets
-  let skillData = dataset1;
-  let techData = dataset2;
-  let projData = dataset3;
+  const MIN_WIDTH = 660;
+  const MAX_HEIGHT = 1250; //increase if you want really large viz on screens
+  const PADDING = 24; //in scale of 8th
+  const MAX_TECH_NODE = 16;
+  const MAX_SKILL_NODE = 24;
+
+  //proportional scaling
+  let DONUT_WIDTH = max(width, height) - min(width, height);
+  let DONUT_RADIUS = round(DONUT_WIDTH / 2); //max dimension of the skill donut
+  let BOUNDARY_RADIUS = round(
+    (height / 2 - DONUT_RADIUS - PADDING) / 2 + DONUT_RADIUS
+  ); //max dimension of the chart
+  let PROJECTS_RADIUS = round(DONUT_RADIUS - (DONUT_RADIUS / 4 + 2 * PADDING)); //Project ring dimension
+  let TECHNOLOGY_RADIUS = round(
+    (PROJECTS_RADIUS - PADDING) / 2 + PROJECTS_RADIUS / 4
+  ); //Tech Circles domains
+
+  //constructor circles
+  let CENTRAL_HOLE_RADIUS = round(BOUNDARY_RADIUS / 10); //empty central space
+  let SKILL_BOUNDARY_RADIUS = round(
+    (BOUNDARY_RADIUS - DONUT_RADIUS - PADDING) / 2
+  );
+
+  //////set scales
+  //quali?
+  //-> skill nodes, tech nodes, angles (?)
+
+  //open point -> serve da capire la questione dell'offset del donut ring
+
+  /////////////////////DATASETS///////////////////////
 
   //unique lists
   let skills, //skill, frequency
@@ -58,19 +90,21 @@ const createCompetencesMap = (container, dataset1, dataset2, dataset3) => {
     repTech, //same
     dissTech, //same
     projects, //uniqueId
-    skillType; //uniqueId, angle
+    skillType = []; //uniqueId, angle
 
   //calc list
   let skills_in_skillType, //for type angle
-    skills_in_Projects; //for skill radius
+    skills_in_Projects = []; //for skill radius
 
   //for simulations
-  let nodes_skillToproj, nodes_projTotech;
-  let edges_skillToproj, edges_projTotech;
+  let nodes_skillToproj,
+    nodes_projTotech = [];
+  let edges_skillToproj,
+    edges_projTotech = [];
 
   ////////////hover/clicked variables
-  let vizProj;
-  let clicked; //collect clicked nodes -- need to collect related?
+  let vizProj,
+    clicked = []; //collect clicked nodes -- need to collect related?
 
   //modals + html variables
   let nodeModal, projModal;
@@ -82,40 +116,14 @@ const createCompetencesMap = (container, dataset1, dataset2, dataset3) => {
   //colors
   const COLORS = {
     background: "#f5f5f0",
-    //add others
   };
-
-  /***
-   * GEOM
-   * raggio proj -> (spessore + buco interno = derivati)
-   * padding proj
-   *    raggio tech (derivato= raggio proj - padding proj)
-   *        settori triangolari
-   * raggio minimo vuoto centrale (da togliere a raggio tech)
-   * raggio skilltype -> (spessore + buco interno = derivati)
-   * angolo
-   *    raggio padding esterno + raggio padding interno (o offset da capire) <-
-   *        mid point interno, midpoint esterno
-   *        centro Cerchi simulation
-   *      -> (buco interno determinato da padding)
-   ***/
-
-  /////////set sizes
-  const MIN_WIDTH = 660;
-  let width = max(MIN_WIDTH, container.clientWidth);
-  let height = container.clientHeight;
-
-  let BOUNDARY_RADIUS = width / 2; //facendo così si riesce ad ottenere una versione "responsive"
-  let PROJECTS_RADIUS;
-  let TECHNOLOGY_RADIUS;
-  let DONUT_RADIUS;
-
-  let angle;
 
   /////////////////////FOR VISUALISATION///////////////////////
 
-  //create svg container
-  const svg = d3
+  //simulations
+  let skill_sim, tech_sim;
+
+  let svg = d3
     .select(container)
     .append("svg")
     .attr("id", "data-visualisation")
@@ -125,14 +133,58 @@ const createCompetencesMap = (container, dataset1, dataset2, dataset3) => {
     .style("background-color", COLORS.background)
     .style("margin", "0");
 
-  //to handle resizing automatically
-  //https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
-  // to see later
+  let g = svg.append("g");
 
-  const g = svg
-    .append("g")
-    .attr("transform", `translate(${width / 2}, ${height / 2})`);
+  //////////////////////////////////
+  ////// Manage Datasets //////////
+  /////////////////////////////////
 
-  //simulations
-  let skill_sim, tech_sim;
+  function prepareData(s, t, p) {
+    //dataset logic
+    /**
+     *
+     */
+  } //prepareData()
+
+  //////////////////////////////////
+  ////// Drawing functions ////////
+  /////////////////////////////////
+
+  function draw() {
+    g.selectAll("*").remove(); //clean the visualisation
+
+    //call specific drawing and simulation functions;
+
+    //call simulations
+    //call hover logic
+  } //draw()
+
+  function chart(skillData, techData, projData) {
+    //create svg container
+    //data logic
+    //draw logic
+    prepareData(skillData, techData, projData);
+    chart.resize();
+  } //chart())
+
+  chart.width = function (value) {
+    if (!arguments.length) return width;
+    width = max(MIN_WIDTH, value);
+    return chart; //method chaining
+  }; //define width chart
+
+  chart.height = function (value) {
+    if (!arguments.length) return height;
+    height = min(MAX_HEIGHT, value);
+    return chart;
+  }; //define height chart
+
+  chart.resize = function () {
+    svg.attr("width", width).attr("height", height);
+    g.attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+    draw();
+  }; //handle resizes
+
+  return chart;
 };
